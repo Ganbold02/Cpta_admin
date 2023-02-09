fe45af1e9c7d74d01072fe7107f752f745bac0c8
Ganbold02 <115781781+Ganbold02@users.noreply.github.com>
2023-02-09 13:20:05 +0800

    2

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import adminDashboard from './Page/adminDashboard';
+
+function App() {
+  return (
+  <>
+    <adminDashboard/>
+  </>
+  );
+}
+
+export default App;
---
+++ src/Page/adminDashboard.jsx
@@ -0,0 +1,13 @@
+export default function adminDashboard(){
+    return(
+    <>
+        <div className="container">
+            <H1>hi</H1>
+            <div className="col-ms-3 ColL"></div>
+            <div className="col-ms-12 colR"></div>
+        </div>
+
+    
+    </>
+    )
+}
